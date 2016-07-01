02bd65d40f740b8cce6c739f5161662fc2a0c0c4
Midaysa Palacios <mida.palacios@gmail.com>
2016-07-01 13:09:32 +0000

    Added reading files ability

--- SolverRPP.java
+++ SolverRPP.java
@@ -1,18 +1,110 @@
 import java.util.*;
-
+import java.lang.Math.*;
 
 public class SolverRPP {
+    private EdgeWeightedGraph G;
+    private HashSet<Edge> R;
+    private String algorithm;
 
-    public SolverRPP(In in) {
+
+    public SolverRPP(In in, String used_algorithm) {
+        int a, u, v, V, requiredE, notRequiredE, useless_int;
+        double weigth;
+        char useless_char;
+        String useless_string, str;
         
+        algorithm = used_algorithm;  // -g or -s
+        useless_string = in.readLine();
+        //System.out.println("useless: " + useless_string);
+        useless_string = in.readLine();
+        //System.out.println("useless: " + useless_string);
+        useless_string = in.readString();
+        //System.out.println("useless: " + useless_string);
+        useless_string = in.readString();
+        //System.out.println("useless: " + useless_string);
+        V = in.readInt();
+        //System.out.println("V = " + V);
+        useless_string = in.readString();
+        //System.out.println("useless: " + useless_string);
+        useless_string = in.readString();
+        //System.out.println("useless: " + useless_string);
+        requiredE = in.readInt();
+        //System.out.println("requiredE " + requiredE);
+        useless_string = in.readString();
+        //System.out.println("useless: " + useless_string);
+        useless_string = in.readString();
+        //System.out.println("useless: " + useless_string);
+        notRequiredE = in.readInt();
+        //System.out.println("notRequiredE" + notRequiredE);
+        useless_string = in.readString();
+        //System.out.println("useless: " + useless_string);
+        useless_string = in.readString();
+        //System.out.println("useless: " + useless_string);
+        
+        G = new EdgeWeightedGraph(V+1);
+        R = new HashSet<Edge>();
+
+        for (int i = 0; i < requiredE; i++) {
+            useless_string = in.readString();
+            //System.out.println("useless_string " + useless_string);
+            
+            str = in.readString();  // number with a comma at the end
+            u = Integer.parseInt(str.substring(0, str.length() - 1));
+            //System.out.println(u);
+            
+            str = in.readString();  // number with a comma at the end
+            v = Integer.parseInt(str.substring(0, str.length() - 1));
+            //System.out.println(v);
+            
+            useless_string = in.readString();
+            //System.out.println("useless_string " + useless_string);
+            
+            weigth = in.readInt();
+            //System.out.println("weigth " + weigth);
+            useless_int = in.readInt();
+            //System.out.println("useless_int " + useless_int);
+            
+            Edge e = new Edge(u, v, weigth);
+            G.addEdge(e);
+            R.add(e);
+        }
+        
+        useless_string = in.readString();
+        //System.out.println("useless: " + useless_string);
+        useless_string = in.readString();
+        //System.out.println("useless: " + useless_string);
+
+        for (int i = 0; i < requiredE; i++) {
+            useless_string = in.readString();
+            //System.out.println("useless_string " + useless_string);
+            
+            str = in.readString();  // number with a comma at the end
+            u = Integer.parseInt(str.substring(0, str.length() - 1));
+            //System.out.println(u);
+            
+            str = in.readString();  // number with a comma at the end
+            v = Integer.parseInt(str.substring(0, str.length() - 1));
+            //System.out.println(v);
+            
+            useless_string = in.readString();
+            //System.out.println("useless_string " + useless_string);
+            
+            weigth = in.readInt();
+            //System.out.println("weigth " + weigth);
+            useless_int = in.readInt();
+            //System.out.println("useless_int " + useless_int);
+            
+            Edge e = new Edge(u, v, weigth);
+            G.addEdge(e);
+        }
     }
 
 
-    public ArrayList<Integer> GetSolution(EdgeWeightedGraph G, HashSet<Edge> R) {
-        EdgeWeightedGraph Gr = new EdgeWeightedGraph(G.V());
+    public ArrayList<Integer> GetSolution() {
+        EdgeWeightedGraph Gr = new EdgeWeightedGraph(this.G.V());
         EulerianCycle EC;
         
-        for (Edge edge : R)
+        for (Edge edge : this.R)
             Gr.addEdge(edge);
         
         Floyd CCM;
@@ -60,6 +152,8 @@ public class SolverRPP {
         ArrayList<Double> CCM_dist_list;
         int node_u, node_v;
         
+        System.out.println(CCM.dist(297, 357));
+        
         // build complete graph Gt using Gr's connected components as big nodes 
         // and costs taken from G's paths
         for (int i=0; i<CC_list.size(); i++) { // Connected Component i
@@ -83,6 +177,7 @@ public class SolverRPP {
                     }
                     // add an edge from CC[i] to CC[j] with a cost of
                     // min(CCM_dist_list) to the new graph Gt
+                   
                     Gt.addEdge(new Edge(i, j, Collections.min(CCM_dist_list)));
                 }
             }
@@ -120,23 +215,26 @@ public class SolverRPP {
             }
         }
         
-        HashSet<Edge> MG = new HashSet<Edge>();
-        GreedyPerfectMatch gpm = new GreedyPerfectMatch();
-        MG = gpm.PerfectMatching(Go);
-        
-        for (Edge e : MG) {
-            for (Edge s: Go.edges()) {
-                Gr.addEdge(s);
+        if (this.algorithm.equals("g")) {
+            HashSet<Edge> MG = new HashSet<Edge>();
+            GreedyPerfectMatch gpm = new GreedyPerfectMatch();
+            MG = gpm.PerfectMatching(Go);
+            
+            for (Edge e : MG) {
+                for (Edge s: Go.edges()) {
+                    Gr.addEdge(s);
+                }
             }
         }
-        
-        HashSet<Edge> MV = new HashSet<Edge>();
-        VertexScanPerfectMatch vspm = new VertexScanPerfectMatch();
-        MV = vspm.PerfectMatching(Go);
-        
-        for (Edge e : MV) {
-            for (Edge s: Go.edges()) {
-                Gr.addEdge(s);
+        else if (this.algorithm.equals("s")) {
+            HashSet<Edge> MV = new HashSet<Edge>();
+            VertexScanPerfectMatch vspm = new VertexScanPerfectMatch();
+            MV = vspm.PerfectMatching(Go);
+            
+            for (Edge e : MV) {
+                for (Edge s: Go.edges()) {
+                    Gr.addEdge(s);
+                }
             }
         }
     }
@@ -153,11 +251,15 @@ public class SolverRPP {
     
     
     public static void main(String[] args) {
-        In in = new In(args[0]);
-        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
-        HashSet<Edge> R = new HashSet<Edge>();
+        if (args.length != 2) {
+            System.out.println("Usage: \nSolverRPP [-g] [-s] <problem instance>");
+            System.exit(1);
+        }
         
-        RuralPostmanProblem rpp = new RuralPostmanProblem();
-        System.out.println(rpp.GetSolution(G, R));
+        In in = new In(args[1]);
+        long initial_time = System.nanoTime();
+        SolverRPP rpp = new SolverRPP(in, args[0]);
+        System.out.println(rpp.GetSolution());
+        System.out.println(System.nanoTime() - initial_time);
     }
 }
